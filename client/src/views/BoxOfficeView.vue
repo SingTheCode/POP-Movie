170c9b0cbfb3d77768382678c9d126a73c6a980d
spiderq2697 <spiderq10@gmail.com>
2022-05-23 20:25:59 +0900

    feat: BoxOfficeView에서 BoxOfficeItem을 추가한다

--- client/src/views/BoxOfficeView.vue
+++ client/src/views/BoxOfficeView.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="box-office">
-    <PosterList></PosterList>
+    <box-office-item :boxOfficeItem="boxOfficeItem"></box-office-item>
+    <poster-list :posterSrcList="posterSrcList"></poster-list>
   </div>
 </template>
 
 <script>
+import BoxOfficeItem from "@/components/BoxOfficeItem";
 import PosterList from "@/components/PosterList";
 
 export default {
   name: "BoxOffice",
   components: {
+    BoxOfficeItem,
     PosterList,
-  }
-}
+  },
+  computed: {
+    posterSrcList: function () {
+      return this.$store.state.boxOffices.posterSrc;
+    },
+    boxOfficeItem: function () {
+      const currentIdx = this.$store.state.currentMovieIdx;
+      return this.$store.state.boxOffices[currentIdx];
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+#box-office {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
