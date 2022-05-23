373d2ed3557933a8634ff13b7b1e991c10d66ac5
koyohan <hphk.rdg9074@gmail.com>
2022-05-23 21:06:22 +0900

    login 보완

--- server/movies/serializers/movie.py
+++ server/movies/serializers/movie.py
@@ -4,20 +4,20 @@ from ..models import Movie
 from .comment import CommentSerializer
 
 
-# class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     
-#     class UserSerializer(serializers.ModelSerializer):
-#         class Meta:
-#             model = get_user_model()
-#             fields = ('pk', 'username')
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('pk', 'username')
 
-#     comments = CommentSerializer(many=True, read_only=True)
-#     user = UserSerializer(read_only=True)
-#     like_users = UserSerializer(read_only=True, many=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True)
+    like_users = UserSerializer(read_only=True, many=True)
 
-#     class Meta:
-#         model = Movie
-#         fields = ('pk', 'user', 'title', 'content', 'comments', 'like_users')
+    class Meta:
+        model = Movie
+        fields = ('pk', 'user', 'title', 'content', 'comments', 'like_users')
 
 
 class MovieListSerializer(serializers.ModelSerializer):
