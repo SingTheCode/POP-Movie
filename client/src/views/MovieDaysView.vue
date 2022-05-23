d1ac2c4a57d3ed2498864bd6e066ad53e9b03c00
spiderq2697 <spiderq10@gmail.com>
2022-05-23 13:28:00 +0900

    feat: view를 만들고 라우팅 설정한다

---
+++ client/src/views/MovieDaysView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="movie-days">
+    Movie Days
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieDays"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/MyPageView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="my-page">
+    My Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyPageView"
+}
+</script>
+
+<style scoped>
+
+</style>
