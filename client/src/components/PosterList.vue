de55cdff4e298b004eb67c03186c7fa32c54b1e7
spiderq2697 <spiderq10@gmail.com>
2022-05-23 13:26:44 +0900

    feat: 포스터 캐러셀을 구현한다

---
+++ client/src/components/PosterList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="poster-list">
+    <swiper ref="mySwiper" :options="swiperOption">
+      <swiper-slide><img
+        src="https://swiperjs.com/demos/images/nature-1.jpg" alt="poster"/></swiper-slide>
+      <swiper-slide><img
+          src="https://swiperjs.com/demos/images/nature-1.jpg" alt="poster"/></swiper-slide>
+      <swiper-slide><img
+          src="https://swiperjs.com/demos/images/nature-1.jpg" alt="poster"/></swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+export default {
+  data () {
+    return {
+      swiperOption: {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: true,
+      }
+    }
+  },
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  }
+}
+</script>
+
+<style scoped>
+  #poster-list {
+    width: 50vw;
+  }
+
+  .swiper {
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 300px;
+    height: 300px;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+  }
+</style>
