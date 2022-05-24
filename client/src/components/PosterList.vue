0a2f56abd8d38d80f4218d8e5ce29799a7005cb6
spiderq2697 <spiderq10@gmail.com>
2022-05-24 09:19:13 +0900

    feat: PosterList.vue에 스타일을 적용한다

--- client/src/components/PosterList.vue
+++ client/src/components/PosterList.vue
@@ -2,14 +2,14 @@
   <div id="poster-list">
     <swiper ref="mySwiper" :options="swiperOption" @slideChange="slideChangeTransitionStart">
       <swiper-slide v-for="(src, index) in posterSrcList" :key="index"
-        ><img :src="src" alt="poster"
+      ><img :src="src" alt="poster"
       /></swiper-slide>
     </swiper>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
+import {Swiper, SwiperSlide, directive} from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
 
 export default {
@@ -56,11 +56,16 @@ export default {
 
 <style scoped>
 #poster-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 50vw;
+  height: 70vh;
 }
 
 .swiper {
   width: 100%;
+  height: 100%;
   padding-top: 50px;
   padding-bottom: 50px;
 }
@@ -69,7 +74,7 @@ export default {
   background-position: center;
   background-size: cover;
   width: 300px;
-  height: 300px;
+  height: 400px;
 }
 
 .swiper-slide img {
