336011c601de2f1238aaa6256a8d435be81bbb7c
spiderq2697 <spiderq10@gmail.com>
2022-05-24 15:23:36 +0900

    feat: 시대별 영화목록 상태를 전역적으로 관리한다

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -10,7 +10,8 @@ export default new Vuex.Store({
   state: {
     currentMovieIdx: 0,
     boxOffices: [],
-    movieDecade: [],
+    randomMovie: {},
+    moviesInDecade: {},
   },
   getters: {},
   mutations: {
@@ -20,12 +21,24 @@ export default new Vuex.Store({
     SET_BOXOFFICES(state, boxOffices) {
       state.boxOffices = boxOffices;
     },
+    SET_MOVIEDAYS(state, movieData) {
+      state.randomMovie = movieData.randomMovie;
+      Object.keys(movieData.moviesInDecade).forEach((key) => {
+        state.moviesInDecade[key] = movieData.moviesInDecade[key];
+      });
+    },
   },
   actions: {
     fetchBoxOffices({ commit }) {
       axios
         .get(SERVER.URL + SERVER.ROUTES.boxOffices)
         .then((res) => commit("SET_BOXOFFICES", res.data))
+        .catch((err) => console.error(err.res.data));
+    },
+    fetchMovieDays({ commit }) {
+      axios
+        .get(SERVER.URL + SERVER.ROUTES.movieDays)
+        .then((res) => commit("SET_MOVIEDAYS", res.data))
         .catch((err) => console.error(err.res.data));
     },
   },
