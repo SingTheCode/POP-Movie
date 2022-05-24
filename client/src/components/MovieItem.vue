a3c752f9fb2b51575f119741c32a7e6fe4e29d35
spiderq2697 <spiderq10@gmail.com>
2022-05-24 15:23:51 +0900

    feat: MovieItem을 추가한다

---
+++ client/src/components/MovieItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="movie-item">
+    <img :src="movie.posterSrc"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieItem",
+  props: {
+    movie: Object,
+  },
+  mounted() {
+    console.log(this.movie)
+  }
+};
+</script>
+
+<style scoped>
+#movie-item {
+  display: block;
+  width: 100%;
+}
+</style>
