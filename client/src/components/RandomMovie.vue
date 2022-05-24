16b71ed3d33950c1d85d0593b6e4451a377a9710
spiderq2697 <spiderq10@gmail.com>
2022-05-24 15:22:39 +0900

    feat: Random Movie를 보여준다

---
+++ client/src/components/RandomMovie.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="random-movie">
+    <img :src="posterSrc" alt="random movie">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RandomMovie",
+  computed: {
+    posterSrc: function () {
+      return this.$store.state.randomMovie.posterSrc;
+    },
+    posterId: function () {
+      return this.$store.state.randomMovie.id;
+    }
+  }
+}
+</script>
+
+<style scoped>
+#random-movie {
+  position: relative;
+  top: 3rem;
+  width: 100vw;
+  height: 45vh;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
