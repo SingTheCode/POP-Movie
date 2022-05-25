a4bf4be6fff026ecbf7f87364e4cdaaf5a304149
spiderq2697 <spiderq10@gmail.com>
2022-05-25 12:46:36 +0900

    feat: 예고편 영상 스트리밍을 생성한다

---
+++ client/src/components/MovieTrailer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="movie-trailer">
+    <iframe src="https://www.youtube.com/embed/mI9oyFMUlfg?controls=0"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieTrailer"
+}
+</script>
+
+<style scoped>
+#movie-trailer {
+  position: relative;
+  top: 3rem;
+  padding-bottom: 56.25%;
+}
+
+iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
