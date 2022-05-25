abca3e4f8dc91fa9bed7b6a363456f67fc682cd1
spiderq2697 <spiderq10@gmail.com>
2022-05-25 12:44:41 +0900

    feat: 영화 상세페이지를 생성한다

--- client/src/views/MovieDetailView.vue
+++ client/src/views/MovieDetailView.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="movie-detail">
-    Movie Detail
+    <MovieTrailer/>
+    <DetailInfo/>
   </div>
 </template>
 
 <script>
+import MovieTrailer from "@/components/MovieTrailer";
+import DetailInfo from "@/components/DetailInfo";
+
 export default {
-  name: "MovieDetailView"
+  name: "MovieDetailView",
+  components: {
+    MovieTrailer,
+    DetailInfo,
+  },
+  created() {
+    const movieId = this.$route.params.id;
+    this.$store.dispatch("fetchMovieDetail", movieId);
+  },
 }
 </script>
 
 <style scoped>
-
+#movie-detail {
+  width: 60vw;
+  height: 100vh;
+}
 </style>
